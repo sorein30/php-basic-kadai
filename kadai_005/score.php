a4fdf3e2644dcc734e3cba639dbeec5aab37555b
osoiremin <mio_flute_1223@yahoo.co.jp>
2024-08-04 16:36:06 +0900

    commit

---
+++ kadai_005/score.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset='UTF-8' />
+  <title>テストの平均点を計算しよう</title>
+  </head>
+
+<body>
+  <p>
+    <?php
+    // 変数に値を代入する
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+    
+    //  合計点を計算する
+    $total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+    echo $total;
+    // 平均点を計算する
+    $ave = $total / 10;
+    echo "<br>";
+    echo $ave;
+    ?>
+    </p>
+
+</body>
+
+</html>
