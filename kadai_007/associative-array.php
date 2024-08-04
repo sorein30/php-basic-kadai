030723d260cc5c1aa8b6fd22d249dedf26016818
osoiremin <mio_flute_1223@yahoo.co.jp>
2024-08-04 17:16:45 +0900

    commit

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset='UTF-8' />
+  <title>連想配列を作って値とキーを出力しよう</title>
+  </head>
+
+<body>
+  <p>
+    <?php
+    // 変数に値を代入する
+    $vage = ['name' => 'onion', 'price' => '200' , 'weight' => '160' ];
+    
+    // 連想配列の中身を出力する
+    print_r($vage);
+    ?>
+    </p>
+
+</body>
+
+</html>
